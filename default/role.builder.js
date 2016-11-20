c52193072ec86b8833e152d6a573bfe82d1601cd
Simon Jentsch <simon.jentsch@9cookies.com>
2016-11-20 15:51:42 +0100

    first files

---
+++ default/role.builder.js
@@ -0,0 +1,15 @@
+module.exports = {
+    run: function (creep) {
+    	if(creep.energy == 0) {
+    		creep.moveTo(Game.spawns.Spawn1);
+    		Game.spawns.Spawn1.transferEnergy(creep);
+    	}
+    	else {
+    		var targets = creep.room.find(Game.CONSTRUCTION_SITES);
+    		if(targets.length) {
+    			creep.moveTo(targets[0]);
+    			creep.build(targets[0]);
+    		}
+    	}
+    }
+}
