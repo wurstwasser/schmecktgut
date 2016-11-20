3a73f6f60c14a74a31df098540eabb9c6b02bf91
Simon Jentsch <simon.jentsch@9cookies.com>
2016-11-20 18:38:39 +0100

    fix builder path bug

--- default/role.builder.js
+++ default/role.builder.js
@@ -13,10 +13,10 @@ module.exports = {
     }
 
     if (creep.memory.building) {
-      var targets = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
-      if (targets.length) {
-        if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(targets[0]);
+      var target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+      if (target) {
+        if (creep.build(target) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(target);
         }
       }
     } else {
