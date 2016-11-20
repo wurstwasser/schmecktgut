d7a559acfcf038a1eeb80f9b3a26e187466e1f56
Simon Jentsch <simon.jentsch@9cookies.com>
2016-11-20 20:05:37 +0100

    let harvesters fill nearest energy structure

--- default/role.harvester.js
+++ default/role.harvester.js
@@ -1,24 +1,56 @@
 const collectEnergy = require('job.collectEnergy');
 
+const SUPPLIED_STRUCTURES = [[STRUCTURE_SPAWN, STRUCTURE_EXTENSION], [STRUCTURE_STORAGE]]
+
 module.exports = {
 
   /** @param {Creep} creep **/
   run: function(creep) {
-    const spawn = creep.memory.spawn;
-
     if (creep.carry.energy < creep.carryCapacity) {
       collectEnergy(creep);
-    } else if (Game.spawns[spawn].energy < Game.spawns[spawn].energyCapacity) {
-      if (creep.transfer(Game.spawns[spawn], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(Game.spawns[spawn]);
-      }
     } else {
-      return -1;
+      let target;
+      let targets = creep.room.find(FIND_STRUCTURES, {
+        filter: (structure) => {
+          let result = fals^e;
+          _.flatten(SUPPLIED_STRUCTURES).forEach((struct) => result = result || structure.structureType === struct);
+          return result && structure.energy < structure.energyCapacity;
+        }
+      });
+
+      targets = _.groupBy(targets, (structure) => {
+        for (let i = 0; i < SUPPLIED_STRUCTURES.length; i++) {
+          const structureGroup = SUPPLIED_STRUCTURES[i];
+          for (const groupStructure of structureGroup) {
+            if (structure.structureType === groupStructure) {
+              return i;
+            }
+          }
+        }
+      });
+
+      for (targetGroup of targets) {
+        if (targetGroup.length === 0) continue;
+        else {
+          target = creep.pos.findClosestByPath(targetGroup);
+          break;
+        };
+      }
+
+      if (target) {
+        if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(target);
+        }
+      }
     }
   },
   bodies: {
-    default: [WORK,CARRY,MOVE],
-    base: [WORK,CARRY,MOVE],
-    extend: [WORK, WORK, CARRY, MOVE],
-  }
+    default: [
+      WORK, CARRY, MOVE,
+    ],
+    base: [
+      WORK, CARRY, MOVE,
+    ],
+    extend: [WORK, WORK, CARRY, MOVE,]
+  },
 };
