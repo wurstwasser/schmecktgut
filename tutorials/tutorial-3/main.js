0ef2f3ecb22280a18565adcde371f7927511b7eb
Johannes Merz <jmerz@hyphe.me>
2016-11-20 17:17:20 +0100

    feat tutorials

---
+++ tutorials/tutorial-3/main.js
@@ -0,0 +1,11 @@
+var roleHarvester = require('role.harvester');
+
+module.exports.loop = function () {
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+    }
+}
