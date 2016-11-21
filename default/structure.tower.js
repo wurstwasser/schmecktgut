ee5a4b8a218480e22ccf751a9c46ccc75238c269
Johannes Merz <jmerz@hyphe.me>
2016-11-21 23:35:21 +0100

    disables tower repair for now

--- default/structure.tower.js
+++ default/structure.tower.js
@@ -1,4 +1,4 @@
-const REPAIR_ENERGY_RATIO = 0.5;
+// const REPAIR_ENERGY_RATIO = 0.5;
 
 module.exports = {
   run: function(tower) {
@@ -7,14 +7,14 @@ module.exports = {
       tower.attack(closestHostile);
       return;
     }
-    if(tower.energy > tower.energyCapacity * REPAIR_ENERGY_RATIO)
-    var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-      filter: (structure) => structure.hits < structure.hitsMax
-    });
-    if (closestDamagedStructure) {
-      tower.repair(closestDamagedStructure);
-      return;
-    }
+    // if(tower.energy > tower.energyCapacity * REPAIR_ENERGY_RATIO)
+    // var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+    //   filter: (structure) => structure.hits < structure.hitsMax
+    // });
+    // if (closestDamagedStructure) {
+    //   tower.repair(closestDamagedStructure);
+    //   return;
+    // }
 
   }
 };
